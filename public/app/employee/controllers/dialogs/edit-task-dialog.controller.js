258a78f1ad34dca7f086effcb9b2cc854b83f263
Marco Paco <marcopaco1230@gmail.com>
2017-04-18 18:09:38 +0800

    Adding edit task dialog to updated designers view

--- public/app/employee/controllers/dialogs/edit-task-dialog.controller.js
+++ public/app/employee/controllers/dialogs/edit-task-dialog.controller.js
@@ -1,10 +1,47 @@
 employee
-	.controller('editTaskDialogController', ['MaterialDesign', 'taskFormService', 'formService', function(MaterialDesign, taskFormService, formService){
+	.controller('editTaskDialogController', ['MaterialDesign', 'taskFormService', 'formService', 'Account', 'User', function(MaterialDesign, taskFormService, formService, Account, User){
 		var vm = this;
 
 		vm.task = taskFormService;
+		vm.account = Account;
+		vm.user = User;
+
+		vm.department = vm.user.user.department.name;
+
+		// determines the user if he can use batch tasks
+		if(vm.department == 'Revolve')
+		{
+			vm.batchable = true;
+		}
+
+		// determines the user if he can use batch tasks
+		if(vm.task.data.number_of_photos)
+		{
+			vm.batch = true;
+		}
+		
+		// fetch the accounts associated with the user
+		vm.accounts = function(){
+			var query = {
+				where: [
+					{
+						column: 'department_id',
+						condition: '=',
+						value: vm.user.user.department_id,
+					}
+				],
+			}
+
+			vm.account.enlist(query)
+				.then(function(data){
+					vm.account.data = data.data;
+				}, function(){
+					Helper.error();
+				});
+		}
 
 		vm.cancel = function(){
+			vm.task.init();
 			formService.cancel();
 		}
 
@@ -27,10 +64,16 @@ employee
 						MaterialDesign.notify('Changes saved.');
 						
 						MaterialDesign.hide();
+
+						vm.task.init();
 					}, function(){
 						vm.busy = false;
 						MaterialDesign.error();
 					})
 			}
 		}
+
+		vm.init = function(){
+			vm.accounts();
+		}();
 	}]);
