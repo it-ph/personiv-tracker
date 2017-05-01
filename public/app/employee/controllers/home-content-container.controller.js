78bb0ea148f86dda7546c110d1ed2d2b168b3607
Marco Paco <marcopaco1230@gmail.com>
2017-05-01 16:36:56 +0800

    Confirmation message on Finish task

--- public/app/employee/controllers/home-content-container.controller.js
+++ public/app/employee/controllers/home-content-container.controller.js
@@ -52,22 +52,32 @@ employee
 
 		// submit current task as finished
 		vm.finish = function(){
-			MaterialDesign.preloader();
+			var dialog = {
+				title: 'Finish Task',
+				message: 'Are you sure you want to finsih this task? This action cannot be undone.',
+				ok: 'Finish',
+				cancel: 'Cancel',
+			}
 
-			vm.task.finish()
-				.then(function(){
-					MaterialDesign.hide();
+			MaterialDesign.confirm(dialog)
+				.then(function(){			
+					MaterialDesign.preloader();
 
-					MaterialDesign.notify('Task completed.');
-					// remove the task pinned at top
-					vm.task.current = null;
+					vm.task.finish()
+						.then(function(){
+							MaterialDesign.hide();
 
-					vm.task.init();
+							MaterialDesign.notify('Task completed.');
+							// remove the task pinned at top
+							vm.task.current = null;
 
-					vm.paused = false;
-				}, function(){
-					MaterialDesign.error();
-				});
+							vm.task.init();
+
+							vm.paused = false;
+						}, function(){
+							MaterialDesign.error();
+						});
+				})
 		}
 
 		// edit a completed task record
