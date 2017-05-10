98a82d98d60cac3843b09a1ad526d5f3926cb033
Marco Paco <marcopaco1230@gmail.com>
2017-05-10 16:54:48 +0800

    Roles Table Update

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -18,6 +18,12 @@ class RolesTableSeeder extends Seeder
                 'created_at' => Carbon\Carbon::now(),
                 'updated_at' => Carbon\Carbon::now(),
         	],
+            [
+                'name' => 'Department Head',
+                'description' => 'user can view all tasks of own department',
+                'created_at' => Carbon\Carbon::now(),
+                'updated_at' => Carbon\Carbon::now(),
+            ],
         ]);
     }
 }
